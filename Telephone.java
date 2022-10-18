39fe05899ec7cf3d26066e032823814ce0a472b4
kiruthika <kirthimuthu99@gmail.com>
2022-10-18 21:03:39 +0530

    Telephone.java

---
+++ Telephone.java
@@ -0,0 +1,14 @@
+class Telephone
+{
+int price = 1500;
+String name="Phone";
+public void call()
+{
+System.out.println("Calling Facility");
+}
+public void receive_call()
+{
+System.out.println("Receiving call facility");
+
+}
+}
